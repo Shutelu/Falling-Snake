16ac9ba42135ebe3e79320cbddd0581808578373
Shutelu <changkai.wang@outlook.com>
2022-09-29 20:06:18 +0200

    Modif App.java, adding default configuration

--- App.java
+++ App.java
@@ -1,7 +1,25 @@
+import javax.swing.JFrame;
+
 public class App{
 
     public static void main(String[] args){
-        //TODO
+        
+        //Variables creation 
+        JFrame mainWindow = new JFrame();
+        int mainWindowSizeX = 400;
+        int mainWindowSizeY = 400;
+        int mainWindowLocationX = 600;
+        int mainWindowLocationY = 200;
+
+        //mainWindow configuration
+        mainWindow.setTitle("Failling Snake");
+        mainWindow.setSize(mainWindowSizeX, mainWindowSizeY);
+        mainWindow.setLocation(mainWindowLocationX, mainWindowLocationY);
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainWindow.setResizable(false);
+        mainWindow.setVisible(true);
+
+        //Main codes
     }
 
 }
