86fb7ede17b23c9db87ee6edfde1e062683a4c0d
Shutelu <changkai.wang@outlook.com>
2022-10-24 16:19:37 +0200

    creation of Entity

---
+++ Entity.java
@@ -0,0 +1,15 @@
+public abstract class Entity {
+    
+    //private -> protected ?
+    private int entity_position_x, entity_position_y;
+    private int entity_width, entity_height;
+    private int entity_move_x, entity_move_y;
+    private boolean entityIsAlive;
+
+
+    //getters
+
+
+    //setters
+
+}
